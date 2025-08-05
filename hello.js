dc75bc5a382ddac349017283f9808cda8b430420
Abdullah Shabir <abdullahabbasi661@gamil.com>
2025-08-05 10:23:27 +0500

    Today I learned about module in node js

---
+++ hello.js
@@ -0,0 +1,13 @@
+// const math = require('./math');
+
+// console.log('Hy I am abdullah abbasi');
+
+
+
+// console.log("Math value is Add : ", math.addFn(5, 6));
+// console.log("Math value is Sub : ", math.subFn(6, 8));
+
+// const math = require("./math");
+// console.log("Math Value is Add", math.add(9, 10));
+// console.log("Math Value is Sub ", math.sub(9, 10));
+
