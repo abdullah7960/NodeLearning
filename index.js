bfda05c474884065a9f74d72fc76bc64f6b0e860
Abdullah Shabir <abdullahabbasi661@gamil.com>
2025-08-21 10:30:55 +0500

    http and url and header and switch

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+    // console.log("New Req Rec.");
+    // console.log(req.headers);
+    const log = `${Date.now()} : ${req.url} New Request Received\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+
+        // res.end("Hello From Server Again");
+        switch (req.url) {
+            case '/': res.end("HomePage");
+                break
+            case '/about': res.end("I am Abdulah");
+                break
+            default
+            '404': res.end("Not foumd")
+        }
+    });
+});
+
+
+
+myServer.listen(8000, () => console.log("Server Started!"));
