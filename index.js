a289b7a93e9838f6130631932f90f49f779a108b
Abdullah Shabir <abdullahabbasi661@gamil.com>
2025-08-25 10:20:02 +0500

    learn about url for getting query details url.parse

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     // console.log("New Req Rec.");
     // console.log(req.headers);
+    if (req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()} : ${req.url} New Request Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
 
         // res.end("Hello From Server Again");
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/': res.end("HomePage");
                 break
-            case '/about': res.end("I am Abdulah");
+            case '/about':
+                const username = myUrl.query.myName;
+
+                res.end(`Hy, ${username}`);
                 break
-            default
-            '404': res.end("Not foumd")
+            default: res.end("Not foumd")
         }
     });
 });
